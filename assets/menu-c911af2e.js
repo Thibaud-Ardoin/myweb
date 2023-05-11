74888c9bdd206351030f224e08e1bf8a05bf1c3a
Thibaud-Ardoin <thibaud.ardoin@ens-paris-saclay.fr>
2023-05-11 11:58:05 +0200

    Updates

---
+++ assets/menu-c911af2e.js
@@ -0,0 +1,3 @@
+console.log(`Hello you :) 
+ Don't hesitate to looks at my github if you like this website. https://github.com/Thibaud-Ardoin/myweb 
+ Give me a ⭐ to let me know you were there ;)`);function n(){fetch("./menu.html").then(e=>e.text()).then(e=>{var t=document.createElement("div");for(t.innerHTML=e;t.children.length>0;)document.getElementsByTagName("main")[0].prepend(t.children[0])})}n();
